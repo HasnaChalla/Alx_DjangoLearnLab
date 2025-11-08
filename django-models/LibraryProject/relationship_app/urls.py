f38dc587d6fa6a3df205b3cfd29acedc7283037a
Hasna_challa <challahasna@gmail.com>
2025-11-09 00:22:17 +0100

    setting up user login, logout, and registration functionalities using Django’s built-in authentication system.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library-detail'),
+    path('login/', views.UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -20,3 +24,21 @@ class LibraryDetailView(DetailView):
         # Get all books in this library using the ManyToManyField relationship
         context['books'] = self.object.books.all()
         return context
+# === AUTHENTICATION VIEWS ===
+
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+
+# Login Django's built-in class-based view
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout Django's built-in class-based view
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('login')  # Redirect after logout
