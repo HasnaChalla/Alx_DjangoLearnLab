9c05b78a1bfedbce6e99c95a40772f18e1a11c07
Hasna_challa <challahasna@gmail.com>
2025-11-09 01:39:27 +0100

    Add UserProfile model with role field and automatic creation via signals

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -32,3 +35,19 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+# Extend the User Model with a UserProfile
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+# Signal to create or update UserProfile whenever User is created
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
