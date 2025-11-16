c5fdb43445eaf616989ec8c2f7c12dde9aefbd97
Hasna_challa <challahasna@gmail.com>
2025-11-16 21:01:15 +0100

    Add Custom User Manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import BaseUserManager
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -10,3 +10,8 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
         date_of_birth = models.DateField('date of birth', null=True, blank=True)
         profile_photo = models.ImageField('profile photo', upload_to='profiles/', null=True, blank=True)
+class CustomUserManager(BaseUserManager):
+      def create_user(self, date_of_birth, profile_photo, password=None, **extra_fields):
+            pass
+      def create_superuser(self, username, email=None, password=None, **extra_fields):
+            pass
