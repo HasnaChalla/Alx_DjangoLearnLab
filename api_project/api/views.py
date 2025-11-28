ae08afdaad99c23405b47e18fb52a9fe1cd9f6a8
Hasna_challa <challahasna@gmail.com>
2025-11-28 21:00:07 +0100

    Build my First API Endpoint with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets, permissions
+from .models import Book 
+from .serializers import BookSerializer 
 # Create your views here.
+class BookList(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
