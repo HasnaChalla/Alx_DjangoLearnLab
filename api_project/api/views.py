97e7266ef1286be4f109c146bc8dcdc14c7ce6b4
Hasna_challa <challahasna@gmail.com>
2025-11-28 21:03:29 +0100

    Extend BookList fron generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets, permissions
-from .models import Book 
-from .serializers import BookSerializer 
+from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
-class BookList(generics.CreateAPIView):
+
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
