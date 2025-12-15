5687ed219e89aa819a0b3ffc6c3d5bfa8338aee9
Hshella <163134999+HasnaChalla@users.noreply.github.com>
2025-12-15 22:28:29 +0100

    Enhance Post and Comment models with tags and ordering

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,26 +1,35 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
+User = settings.AUTH_USER_MODEL
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = TaggableManager(blank=True)
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['-published_date']
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', args=[str(self.pk)])
+
 class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    class Meta:
+        ordering = ['created_at']
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'Comment by {self.author} on {self.post}'
