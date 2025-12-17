a2662d583a847e63530335501296368bf957e5f2
Hasna_challa <challahasna@gmail.com>
2025-12-17 18:49:27 +0100

    Integrate Tagging Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,9 +3,8 @@ from django.db import models
 from django.urls import reverse
 
 
-
 class Tag(models.Model):
-    name  = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -23,9 +22,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    class Comment(models.Model):
-        post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-        author = models.ForeignKey(User, on_delete=models.CASCADE)
-        content = models.TextField()
-        created_at = models.DateTimeField(auto_now_add=True)
-        updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
